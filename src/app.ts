723656ba32c8e18ff0f5cbc8811b931ee2ac78cc
Pallab <pallabkshyp@gmail.com>
2025-06-29 10:40:09 +0530

    server setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello from TypeScript + Express!');
+app.get("/", (_req, res) => {
+  res.send("Hello from TypeScript + Express!");
 });
 
 app.listen(PORT, () => {
